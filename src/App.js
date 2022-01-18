e1aa5c7830ab6ec8dcc3432c44c2c2bd326c74ca
SamyG12 <90658089+SamyG12@users.noreply.github.com>
2022-01-18 09:12:57 -0800

    Deleted unused files. And rearange the folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import About from "./components/About";
+import About from "./components/map/map";
 
 function App() {
   return (
---
+++ src/components/map/map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MapContainer from './MapContainer'
+
+export default function Map() {
+    return (
+        <main>
+           
+            <div>
+                <MapContainer />
+            </div>
+        </main>
+    )
+}
